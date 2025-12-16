8f7a38edf4cb0d98d271f92dc87894b41ee13b00
greencoderx <green.coder18@gmail.com>
2025-12-16 22:57:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,35 +1,37 @@
-import random
+import os
 import json
+import random
+from datetime import datetime
 import tweepy
-from pantun_ai import generate_pantun
+from pantun_ai import generate_pantun, translate_to_english
 
 def load_json(path):
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
 
-hadith = load_json("data/hadith.json")
 quran = load_json("data/quran.json")
 
-def pick_content():
-    choice = random.choices(
-        ["pantun", "hadith", "quran"],
-        weights=[60, 20, 20]
-    )[0]
-
-    if choice == "pantun":
-        return generate_pantun()
-    elif choice == "hadith":
-        return random.choice(hadith)
-    else:
-        return random.choice(quran)
-
 client = tweepy.Client(
-    consumer_key =     os.environ["TW_API_KEY"],
-    consumer_secret =  os.environ["TW_API_SECRET"],
-    access_token =     os.environ["TW_ACCESS_TOKEN"],
+    consumer_key=os.environ["TW_API_KEY"],
+    consumer_secret=os.environ["TW_API_SECRET"],
+    access_token=os.environ["TW_ACCESS_TOKEN"],
     access_token_secret=os.environ["TW_ACCESS_SECRET"]
 )
 
-tweet = pick_content()
-client.create_tweet(text=tweet)
-print("Tweet posted successfully")
+def is_friday():
+    return datetime.utcnow().weekday() == 4
+
+if is_friday():
+    tweet = random.choice(quran)
+    client.create_tweet(text=tweet)
+    print("Friday Qur’an tweet posted")
+else:
+    pantun = generate_pantun()
+    translation = translate_to_english(pantun)
+
+    tweet1 = client.create_tweet(text=pantun)
+    client.create_tweet(
+        text="English translation:\n" + translation,
+        in_reply_to_tweet_id=tweet1.data["id"]
+    )
+    print("Pantun thread posted")
